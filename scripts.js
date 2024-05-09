f104738ca491e8fad24a3833f6291a9610c8d14a
kutlwano10 <kutlwano.ramotebele@gmail.com>
2024-05-09 14:38:12 +0200

    added critical Error Message

--- scripts.js
+++ scripts.js
@@ -9,20 +9,31 @@ form.addEventListener("submit", (event) => {
 
   const errorMessages = {
     NaN: 'Division not performed. Both values are required in inputs. Try again',
-    isZero: 'Division not performed. Invalid number provided. Try again'
+    isZero: 'Division not performed. Invalid number provided. Try again',
+    criticalError: 'Something critical went wrong. Please reload the page'
   }
+  
+  const checkDevidend = 'YOLO';
+  const checkDivider = '+++';
 
-  if(isNaN(dividend) || isNaN(divider) ) {
+
+  //CHECK IF CORRECT INPUT IS ENTERED
+  if (dividend === checkDevidend && divider === checkDivider) {
+    const criticalErrorDiv = document.createElement('div')
+    criticalErrorDiv.classList.add('critical-error')
+
+    criticalErrorDiv.innerHTML = `<p>${errorMessages.criticalError}</p>`
+    document.body.appendChild(criticalErrorDiv)
+
+  }else if(isNaN(dividend) || isNaN(divider) ) {
     result.innerText = errorMessages.NaN
-  }else if (divider == 0){
+  }
+  else if (divider == 0){//=== gives me infinity
     result.innerText = errorMessages.isZero
+    console.error(errorMessages.isZero)
   }
   else {
     result.innerText = Math.floor(dividend / divider);
   }
-  
-  
-    
-  
 
 });
