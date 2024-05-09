126c8a17365ec8d08b999d8f51b88b2eec7fb630
kutlwano10 <kutlwano.ramotebele@gmail.com>
2024-05-09 11:11:44 +0200

    made error messages if isNaN

--- scripts.js
+++ scripts.js
@@ -5,5 +5,21 @@ form.addEventListener("submit", (event) => {
   event.preventDefault();
   const entries = new FormData(event.target);
   const { dividend, divider } = Object.fromEntries(entries);
-  result.innerText = Math.floor(dividend / divider);
+  // result.innerText = Math.floor(dividend / divider);
+
+  
+
+  if(isNaN(dividend) || isNaN(divider) ) {
+    result.innerText = 'Division not performed. Both values are required in inputs. Try again'
+  }else if (divider == 0){
+    result.innerText = 'Division not performed. Invalid number provided. Try again'
+  }
+  else {
+    result.innerText = Math.floor(dividend / divider);
+  }
+  
+  
+    
+  
+
 });
